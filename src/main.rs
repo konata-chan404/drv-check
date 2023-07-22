b9c63124fd097f6476605302bf31b978e1eb61de
Konata <42537566+konata-chan404@users.noreply.github.com>
2023-07-23 02:43:03 +0300

    great additions~

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use std::fs;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use std::fs;
+
+// Add the logger
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
 
 // Include the Report struct and related types and functions from the "report.rs" module
 mod report;
-use report::{Report, ImportSet};
+use report::{default_import_set, ImportSet, Report};
 
 /// CLI options definition using structopt
 #[derive(Debug, StructOpt)]
@@ -13,51 +18,59 @@ struct Opt {
     #[structopt(parse(from_os_str))]
     path: PathBuf,
 
-    /// List of imports to check for
-    #[structopt(short, long)]
-    imports: Vec<String>,
+    /// Path to a JSON file containing the import set (optional)
+    #[structopt(short, long, parse(from_os_str))]
+    imports: Option<PathBuf>,
 }
 
 fn main() {
+    // Initialize the logger
+    pretty_env_logger::init();
+
     // Parse CLI options
     let opt = Opt::from_args();
 
-    // Load the ImportSet from CLI options or use the default set if none provided
-    let import_set: ImportSet = if !opt.imports.is_empty() {
-        opt.imports
-    } else {
-        vec![
-            "MmMapIoSpace".to_string(),
-            "MmMapIoSpaceEx".to_string(),
-            "MmMapLockedPages".to_string(),
-            "MmMapLockedPagesSpecifyCache".to_string(),
-            "MmMapLockedPagesWithReservedMapping".to_string(),
-            "ZwMapViewOfSection".to_string(),
-            // "IoCreateDevice".to_string(), // Uncomment if required
-            // "MmCopyVirtualMemory".to_string(), // Uncomment if required
-            "MmCopyMemory".to_string(),
-            "EnumerateDebuggingDevices".to_string(),
-        ]
+    // Read the import set from the JSON file if provided, otherwise use the default set
+    let import_set = match opt.imports {
+        Some(imports_file) => {
+            let import_set_json =
+                fs::read_to_string(&imports_file).expect("Failed to read import set file");
+            serde_json::from_str(&import_set_json).expect("Failed to parse import set JSON")
+        }
+        None => default_import_set(),
     };
 
     // Call the analyze function with the provided path and import set
     analyze(&opt.path, import_set);
 }
 
+/// Analyzes the driver file or directory and generates reports for each driver.
+///
+/// # Arguments
+///
+/// * `path` - The path to the driver file or directory to analyze.
+/// * `import_set` - The set of import names to check for.
 fn analyze(path: &std::path::Path, import_set: ImportSet) {
+    // Enable verbose logging
+    info!("Starting analysis...");
+
+    let mut reports = Vec::new();
+
     if path.is_file() {
         // Handle single driver file
         match Report::from_driver(path, import_set) {
             Ok(report) => {
-                // Display the report
-                println!("Driver Name: {}", report.name);
-                println!("Driver Hash: {}", report.hash);
-                println!("Found Imports: {:?}", report.found_imports);
-                println!("Matching Imports: {:?}", report.matching_imports);
+                reports.push(report);
             }
             Err(error_code) => {
-                eprintln!("Error: An error occurred with code {}", error_code);
+                // Log the error with file path
+                error!(
+                    "Error {:#?} while analyzing file {}",
+                    error_code,
+                    path.to_string_lossy()
+                );
                 // Handle the error appropriately
+                return;
             }
         }
     } else if path.is_dir() {
@@ -67,17 +80,18 @@ fn analyze(path: &std::path::Path, import_set: ImportSet) {
             if let Ok(entry) = entry {
                 let driver_path = entry.path();
                 if driver_path.is_file() {
-                    println!("Analyzing: {}", driver_path.to_string_lossy());
+                    // Enable verbose logging for each analysis
                     match Report::from_driver(&driver_path, import_set.clone()) {
                         Ok(report) => {
-                            // Display the report
-                            println!("Driver Name: {}", report.name);
-                            println!("Driver Hash: {}", report.hash);
-                            println!("Found Imports: {:?}", report.found_imports);
-                            println!("Matching Imports: {:?}", report.matching_imports);
+                            reports.push(report);
                         }
                         Err(error_code) => {
-                            eprintln!("Error: An error occurred with code {}", error_code);
+                            // Log the error with file path
+                            error!(
+                                "Error {:#?} while analyzing file {}",
+                                error_code,
+                                driver_path.to_string_lossy()
+                            );
                             // Handle the error appropriately
                         }
                     }
@@ -85,6 +99,11 @@ fn analyze(path: &std::path::Path, import_set: ImportSet) {
             }
         }
     } else {
-        eprintln!("Error: The provided path does not exist or is not a file/directory");
+        // Log the error
+        error!("Error: The provided path does not exist or is not a file/directory");
+        return;
     }
+
+    // Print the reports as a JSON list
+    println!("{}", serde_json::to_string_pretty(&reports).unwrap());
 }
--- src/report.rs
+++ src/report.rs
@@ -10,71 +10,113 @@ use pelite::FileMap;
 
 use sha2::{Digest, Sha256};
 
+/// Struct representing a report for a driver analysis.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Report {
+    /// Name of the driver file.
     pub name: String,
+    /// SHA256 hash of the driver file.
     pub hash: String,
+    /// Imports found in the driver that match the provided import set.
     pub found_imports: Vec<Import>,
+    /// Imports from the import set that match those found in the driver.
     pub matching_imports: Vec<Import>,
 }
 
+/// Struct representing an import entry in the driver.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Import {
     #[serde(with = "SerHex::<StrictPfx>")]
+    /// Virtual address of the import.
     pub va: u64,
+    /// Hint of the import.
     pub hint: usize,
+    /// Name of the import.
     pub name: String,
 }
 
+/// Type representing a set of import names.
 pub type ImportSet = Vec<String>;
 
 impl Report {
-    pub fn from_driver(driver: &Path, import_set: ImportSet) -> Result<Report, i32> {
-        let file_map = FileMap::open(driver).map_err(|_| -1)?;
-        let pe = PeFile::from_bytes(file_map.as_ref()).map_err(|_| -1)?;
+    /// Analyzes a driver and generates a report.
+    ///
+    /// # Arguments
+    ///
+    /// * `driver` - The path to the driver file.
+    /// * `import_set` - The set of import names to check for.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` containing the generated report or an error.
+    pub fn from_driver(driver: &Path, import_set: ImportSet) -> Result<Report, ReportError> {
+        // Enable verbose logging for the analysis
+        log::info!("Analyzing driver: {}", driver.to_string_lossy());
 
+        // Open the driver file and map it to memory
+        let file_map = FileMap::open(driver).map_err(|_| ReportError::FileOpenError)?;
+        // Parse the PE file headers
+        let pe = PeFile::from_bytes(file_map.as_ref()).map_err(|_| ReportError::PeFileError)?;
+
+        // Ensure that the driver has the correct subsystem (IMAGE_SUBSYSTEM_NATIVE)
         let nt_headers = pe.nt_headers();
         if nt_headers.OptionalHeader.Subsystem != IMAGE_SUBSYSTEM_NATIVE {
-            return Err(-1);
+            return Err(ReportError::InvalidSubsystem);
         }
 
-        let imports = pe.imports().map_err(|_| -1)?;
+        // Extract the import data from the PE file
+        let imports = pe.imports().map_err(|_| ReportError::ImportsError)?;
 
+        // Prepare lists to store matching and found import entries
         let mut matching_imports = Vec::new();
         let mut found_imports = Vec::new();
 
+        // Iterate through the import data
         for desc in imports {
-            let dll_name = desc.dll_name().unwrap();
+            // Check if the DLL name is "ntoskrnl.exe"
+            let dll_name = desc.dll_name().map_err(|_| ReportError::DllNameError)?;
             if dll_name.to_str().unwrap() != "ntoskrnl.exe" {
                 continue;
             }
 
-            for (va, import) in Iterator::zip(desc.iat().ok().unwrap(), desc.int().ok().unwrap()) {
-                if let Some(import) = import.ok() {
-                    match import {
-                        pelite::pe64::imports::Import::ByName { hint, name } => {
-                            let imp = Import {
-                                va: *va,
-                                hint,
-                                name: name.to_string(),
-                            };
+            // Iterate through the import entries for this DLL
+            for (va, import) in Iterator::zip(
+                desc.iat().map_err(|_| ReportError::IatError)?,
+                desc.int().map_err(|_| ReportError::IntError)?,
+            ) {
+                // Extract the import entry for the IAT and INT
+                let import = import.map_err(|_| ReportError::ImportError)?;
+                match import {
+                    // For Import::ByName, extract the import name and hint
+                    pelite::pe64::imports::Import::ByName { hint, name } => {
+                        // Create a new Import struct for the entry
+                        let imp = Import {
+                            va: *va,
+                            hint,
+                            name: name.to_string(),
+                        };
 
-                            if import_set.contains(&imp.name) {
-                                matching_imports.push(imp);
-                            } else {
-                                found_imports.push(imp);
-                            }
+                        // Check if the import name is in the provided import set
+                        if import_set.contains(&imp.name) {
+                            // If it matches, add it to the matching_imports list
+                            matching_imports.push(imp.clone());
                         }
-                        _ => {}
+                        // Add the import to the found_imports list
+                        found_imports.push(imp);
+                    }
+                    _ => {
+                        // Ignore other import types
                     }
                 }
             }
         }
 
+        // Calculate the SHA256 hash of the driver file
         let mut hasher = Sha256::new();
-        hasher.update(fs::read(driver).unwrap());
+        hasher.update(fs::read(driver).map_err(|_| ReportError::FileReadError)?);
         let hash = format!("{:x}", hasher.finalize_reset());
 
+        // Create a new Report with the analysis results
         Ok(Report {
             name: driver.to_string_lossy().to_string(),
             hash,
@@ -82,4 +124,34 @@ impl Report {
             matching_imports,
         })
     }
+}
+
+/// Custom error enum for the Report module
+#[derive(Debug)]
+pub enum ReportError {
+    FileOpenError,
+    FileReadError,
+    PeFileError,
+    ImportsError,
+    InvalidSubsystem,
+    DllNameError,
+    IatError,
+    IntError,
+    ImportError,
+}
+
+/// Returns the default import set.
+pub fn default_import_set() -> ImportSet {
+    vec![
+        "MmMapIoSpace".to_string(),
+        "MmMapIoSpaceEx".to_string(),
+        "MmMapLockedPages".to_string(),
+        "MmMapLockedPagesSpecifyCache".to_string(),
+        "MmMapLockedPagesWithReservedMapping".to_string(),
+        "ZwMapViewOfSection".to_string(),
+        // "IoCreateDevice".to_string(), // Uncomment if required
+        // "MmCopyVirtualMemory".to_string(), // Uncomment if required
+        "MmCopyMemory".to_string(),
+        "EnumerateDebuggingDevices".to_string(),
+    ]
 }
